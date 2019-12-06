998d604c39d8b1144f0ee00c009b0da6c70d3c98
NHN <congviec18120062@gmail.com>
2019-12-06 11:39:15 +0700

    v1.4.0: Add line
    _ Fix featured row 2 left
    _ Add line conect 2 points

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,13 @@ const color2 = document.getElementById('color2');
 const color3 = document.getElementById('color3');
 const color4 = document.getElementById('color4');
 
+const line = document.getElementById('line');
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
+
 const arr = [0, 0];
+const coor = [0, 0, 0, 0, 0, 0, 0, 0];
 
 function handleMouseMove(event) {
   var dot, eventDoc, doc, body, pageX, pageY;
@@ -62,79 +68,118 @@ body.addEventListener('scroll', function inti(event) {
 function getRandomInt() {
   let min = 0;
   let max = 500;
-  return Math.floor(Math.random() * (max - min + 1)) + min + 'px';
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 var random;
 
 random = getRandomInt();
-color1.style.top = random;
+color1.style.top = random + 'px';
+coor[0] = random + 15;
 random = getRandomInt();
-color1.style.left = random;
+color1.style.left = random + 'px';
+coor[1] = random + 15;
 random = getRandomInt();
-color2.style.top = random;
+color2.style.top = random + 'px';
+coor[2] = random + 15;
 random = getRandomInt();
-color2.style.left = random;
+color2.style.left = random + 'px';
+coor[3] = random + 15;
 random = getRandomInt();
-color3.style.top = random;
+color3.style.top = random + 'px';
+coor[4] = random + 15;
 random = getRandomInt();
-color3.style.left = random;
+color3.style.left = random + 'px';
+coor[5] = random + 15;
 random = getRandomInt();
-color4.style.top = random;
+color4.style.top = random + 'px';
+coor[6] = random + 15;
 random = getRandomInt();
-color4.style.left = random;
+color4.style.left = random + 'px';
+coor[7] = random + 15;
+
+line1.setAttribute('y1', coor[0] + 'px');
+line1.setAttribute('x1', coor[1] + 'px');
+line1.setAttribute('y2', coor[2] + 'px');
+line1.setAttribute('x2', coor[3] + 'px');
+
+line2.setAttribute('y1', coor[2] + 'px');
+line2.setAttribute('x1', coor[3] + 'px');
+line2.setAttribute('y2', coor[4] + 'px');
+line2.setAttribute('x2', coor[5] + 'px');
+
+line3.setAttribute('y1', coor[4] + 'px');
+line3.setAttribute('x1', coor[5] + 'px');
+line3.setAttribute('y2', coor[6] + 'px');
+line3.setAttribute('x2', coor[7] + 'px');
 
 color1.onmouseover = function() {
   man.style.backgroundColor = '#e83890';
   random = getRandomInt();
-  while (random == color1.style.top) {
+  while (random + 'px' == color1.style.top) {
     random = getRandomInt();
   }
-  color1.style.top = random;
+  color1.style.top = random + 'px';
+  line1.setAttribute('y1', random + 15 + 'px');
+
   random = getRandomInt();
-  while (random == color1.style.left) {
+  while (random + 'px' == color1.style.left) {
     random = getRandomInt();
   }
-  color1.style.left = random;
+  color1.style.left = random + 'px';
+  line1.setAttribute('x1', random + 15 + 'px');
 };
 
 color2.onmouseover = function() {
   man.style.backgroundColor = '#e4572e';
   random = getRandomInt();
-  while (random == color2.style.top) {
+  while (random + 'px' == color2.style.top) {
     random = getRandomInt();
   }
-  color2.style.top = random;
+  color2.style.top = random + 'px';
+  line1.setAttribute('y2', random + 15 + 'px');
+  line2.setAttribute('y1', random + 15 + 'px');
+
   random = getRandomInt();
-  while (random == color2.style.left) {
+  while (random + 'px' == color2.style.left) {
     random = getRandomInt();
   }
-  color2.style.left = random;
+  color2.style.left = random + 'px';
+  line1.setAttribute('x2', random + 15 + 'px');
+  line2.setAttribute('x1', random + 15 + 'px');
 };
 
 color3.onmouseover = function() {
   man.style.backgroundColor = '#7678ed';
   random = getRandomInt();
-  while (random == color3.style.top) {
+  while (random + 'px' == color3.style.top) {
     random = getRandomInt();
   }
-  color3.style.top = random;
+  color3.style.top = random + 'px';
+  line2.setAttribute('y2', random + 15 + 'px');
+  line3.setAttribute('y1', random + 15 + 'px');
+
   random = getRandomInt();
-  while (random == color3.style.left) {
+  while (random + 'px' == color3.style.left) {
     random = getRandomInt();
   }
-  color3.style.left = random;
+  color3.style.left = random + 'px';
+  line2.setAttribute('x2', random + 15 + 'px');
+  line3.setAttribute('x1', random + 15 + 'px');
 };
 
 color4.onmouseover = function() {
   man.style.backgroundColor = '#f3b001';
   random = getRandomInt();
-  while (random == color4.style.top) {
+  while (random + 'px' == color4.style.top) {
     random = getRandomInt();
   }
-  color4.style.top = random;
+  color4.style.top = random + 'px';
+  line3.setAttribute('y2', random + 15 + 'px');
+
   random = getRandomInt();
-  while (random == color4.style.left) {
+  while (random + 'px' == color4.style.left) {
     random = getRandomInt();
   }
-  color4.style.left = random;
+  color4.style.left = random + 'px';
+  line3.setAttribute('x2', random + 15 + 'px');
 };
